fdb751740a1a62f623b83a4fc87418ba0cd40343
Bhavya Shah <Bhavya Shah@MCKINSEY.COM>
2016-10-04 10:41:59 -0400

    removed test functions

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,22 +1,3 @@
 var redux = require('redux');
 
 console.log('Starting redux example');
-
-//Pure Function
-function add (a, b) {
-  return a + b;
-}
-
-function changeProp(obj) {
-  return {
-    ...obj,
-    name: 'Jen'
-  };
-}
-
-var res = changeProp({
-  name: 'Bhavya',
-  age: 20
-});
-
-console.log(res);
