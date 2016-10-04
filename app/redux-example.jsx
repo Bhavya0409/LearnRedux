68f27803b54b351f8274ce90419f833909fee466
Bhavya Shah <Bhavya Shah@MCKINSEY.COM>
2016-10-04 13:02:32 -0400

    add actions for adding/removing hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,7 +2,16 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var reducer = (state = {name: 'Anonymous'}, action) => {
+var stateDefault = {
+  name: 'Anonymous',
+  hobbies: [],
+  movies: []
+};
+
+var nextHobbyId = 1;
+var nextMovieId = 1;
+
+var reducer = (state = stateDefault, action) => {
   // state = state || {name: 'Anonymous'};
 
   switch (action.type) {
@@ -10,6 +19,39 @@ var reducer = (state = {name: 'Anonymous'}, action) => {
       return {
         ...state,
         name: action.name
+      };
+    case 'ADD_HOBBY':
+      return {
+        ...state,
+        hobbies: [
+          ...state.hobbies,
+          {
+            id: nextHobbyId++,
+            hobby: action.hobby
+          }
+        ]
+      };
+    case 'REMOVE_HOBBY':
+      return {
+        ...state,
+        hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+      };
+    case 'ADD_MOVIE':
+      return {
+        ...state,
+        movies: [
+          ...state.movies,
+          {
+            title: action.title,
+            genre: action.genre,
+            id: nextMovieId++
+          }
+        ]
+      };
+    case 'REMOVE_MOVIE':
+      return {
+        ...state,
+        movies: state.movies.filter((movie) => movie.id !== action.id)
       };
     default:
       return state;
@@ -20,11 +62,12 @@ var store = redux.createStore(reducer, redux.compose(
 ));
 
 // Subscribe to changes
-var unsubscribe = store.subscribe(() => {
+store.subscribe(() => {
   var state = store.getState();
 
-  console.log('Name is', state.name);
   document.getElementById('app').innerHTML = state.name;
+
+  console.log('New state', store.getState());
 });
 
 var currentState = store.getState();
@@ -36,8 +79,40 @@ store.dispatch({
 });
 
 store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Running'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Walking'
+});
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+});
+
+store.dispatch({
   type: 'CHANGE_NAME',
   name: 'new name'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Movie 1',
+  genre: 'Genre 1'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Movie 2',
+  genre: 'Genre 2'
+});
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 1
 });
 
 // unsubscribe();
