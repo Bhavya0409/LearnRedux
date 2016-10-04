905319e1c2eee4da62e35cea3aef80a75de62407
Bhavya Shah <Bhavya Shah@MCKINSEY.COM>
2016-10-04 11:48:46 -0400

    added redux dev tools and subscribe

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -4,6 +4,7 @@ console.log('Starting redux example');
 
 var reducer = (state = {name: 'Anonymous'}, action) => {
   // state = state || {name: 'Anonymous'};
+
   switch (action.type) {
     case 'CHANGE_NAME':
       return {
@@ -14,7 +15,17 @@ var reducer = (state = {name: 'Anonymous'}, action) => {
       return state;
   }
 };
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+// Subscribe to changes
+var unsubscribe = store.subscribe(() => {
+  var state = store.getState();
+
+  console.log('Name is', state.name);
+  document.getElementById('app').innerHTML = state.name;
+});
 
 var currentState = store.getState();
 console.log('currentState: ', currentState);
@@ -24,4 +35,9 @@ store.dispatch({
   name: 'Bhavya'
 });
 
-console.log('Name should be Bhavya', store.getState())
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'new name'
+});
+
+// unsubscribe();
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -20,13 +20,27 @@ var reducer = (state = stateDefault, action) => {
   }
 };
 
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+var unsubscribe = store.subscribe(() => {
+  var state = store.getState();
+
+  console.log('Search for', state.searchText);
+  document.getElementById('app').innerHTML = state.searchText;
+});
 
 var currentState = store.getState();
 
 store.dispatch({
   type: 'CHANGE_SEARCH_TEXT',
   searchText: 'new search text'
+});
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'new search text 2'
 });
 
 console.log('search text should be "new search text"', store.getState());
